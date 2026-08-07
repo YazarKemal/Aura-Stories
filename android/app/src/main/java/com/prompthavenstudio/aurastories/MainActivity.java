4dfa48b85dd0b7ce0a2a2ec861ad204298e2cccb
YazarKemal <realistfakir1@gmail.com>
2026-08-07 19:51:00 +0300

    register native Aura share plugin

--- android/app/src/main/java/com/prompthavenstudio/aurastories/MainActivity.java
+++ android/app/src/main/java/com/prompthavenstudio/aurastories/MainActivity.java
@@ -1,8 +1,16 @@
 package com.prompthavenstudio.aurastories;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(AuraSharePlugin.class);
+    }
+
     @Override
     public void onBackPressed() {
         if (bridge == null || bridge.getWebView() == null) {
