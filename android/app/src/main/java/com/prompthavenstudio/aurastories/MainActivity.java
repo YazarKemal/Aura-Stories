8c22eb466a47218d030a0b272a6b167a2334c9f7
YazarKemal <realistfakir1@gmail.com>
2026-08-07 19:45:25 +0300

    fix Android hardware back navigation

--- android/app/src/main/java/com/prompthavenstudio/aurastories/MainActivity.java
+++ android/app/src/main/java/com/prompthavenstudio/aurastories/MainActivity.java
@@ -2,4 +2,31 @@ package com.prompthavenstudio.aurastories;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (bridge == null || bridge.getWebView() == null) {
+            performDefaultBack();
+            return;
+        }
+
+        bridge.getWebView().evaluateJavascript(
+            "(function(){" +
+                "var close=document.querySelector('[role=dialog] button[data-aura-close], [role=dialog] button[aria-label=\"Close\"]');" +
+                "if(close){close.click();return 'handled';}" +
+                "var back=document.querySelector('button[aria-label=\"Geri dön\"]');" +
+                "if(back){back.click();return 'handled';}" +
+                "return 'unhandled';" +
+            "})()",
+            result -> {
+                if (!"\"handled\"".equals(result)) {
+                    performDefaultBack();
+                }
+            }
+        );
+    }
+
+    private void performDefaultBack() {
+        super.onBackPressed();
+    }
+}
